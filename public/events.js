c4a4a65bf8387b25b06c5635855e00547d5e5d2e
tawool83 <tawool83@gmail.com>
2026-02-19 14:33:29 +0000

    이벤트 별도 파일 분리

---
+++ public/events.js
@@ -0,0 +1,5 @@
+const defaultEvents = [
+  { age: 45, type: "monthly", amount: 3500000, label: "승진" },
+  { age: 55, type: "lump", amount: 250000000, label: "아파트 매매" },
+  { age: 65, type: "pension", amount: 1800000, label: "국민연금" },
+];
