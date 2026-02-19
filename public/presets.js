c671f03a89d89c36643b109093b267ffd90657b3
tawool83 <tawool83@gmail.com>
2026-02-19 14:16:20 +0000

    프리셋 별도 파일로 분리

---
+++ public/presets.js
@@ -0,0 +1,6 @@
+
+const defaultPresets = [
+  { id: "sp500", name: "S&P 500 (표준)", annualReturnPct: 10.0, dividendPct: 1.5, builtin: true },
+  { id: "nasdaq100", name: "나스닥 100 (기술주 중심)", annualReturnPct: 14.2, dividendPct: 0.8, builtin: true },
+  { id: "schd", name: "SCHD (배당 성장)", annualReturnPct: 8.5, dividendPct: 3.4, builtin: true },
+];
