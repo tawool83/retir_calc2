d6d03ffe89c76b2b4be00f45c555a59a34a75cee
tawool83 <tawool83@gmail.com>
2026-02-20 15:38:24 +0000

    오류수정2

--- public/events.js
+++ public/events.js
@@ -1,23 +1,44 @@
 const defaultEvents = [
   {
-    id: "evt1",
+    id: 'evt_1',
     age: 30,
-    type: "portfolio",
-    presetId: "sp500",
+    type: 'lump',
+    amount: 50000000,
+    label: '초기 투자금'
+  },
+  {
+    id: 'evt_2',
+    age: 30,
+    type: 'monthly',
+    amount: 1500000,
+    label: '초기 월 납입액'
+  },
+  {
+    id: 'evt_3',
+    age: 30,
+    type: 'portfolio',
+    presetId: 'preset_growth_40_dividend_60',
+    weight: 10
+  },
+   {
+    id: 'evt_4',
+    age: 45,
+    type: 'portfolio',
+    presetId: 'preset_growth_20_dividend_80',
     weight: 10
   },
   {
-    id: "evt2",
-    age: 55,
-    type: "portfolio",
-    presetId: "schd",
-    weight: 5
+    id: 'evt_5',
+    age: 65,
+    type: 'monthly',
+    amount: 0, // 은퇴 후 납입 중단
+    label: '은퇴'
   },
   {
-    id: "evt3",
-    age: 55,
-    type: "portfolio",
-    presetId: "sp500",
-    weight: 5
-  } 
+    id: 'evt_6',
+    age: 65,
+    type: 'withdrawal',
+    amount: 3000000,
+    label: '은퇴 후 생활비'
+  }
 ];
