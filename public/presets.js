77752461ebf567fa7c637e6c936d7a0ea05682ca
tawool83 <tawool83@gmail.com>
2026-02-26 06:46:31 +0000

    커뮤니티 추가.

--- public/presets.js
+++ public/presets.js
@@ -1,5 +1,5 @@
 const defaultPresets = [
-  { id: "sp500", name: "S&P 500", annualReturnPct: 6, dividendPct: 1.5, builtin: true },
+  { id: "sp500", name: "S&P 500", annualReturnPct: 6, dividendPct: 1.0, builtin: true },
   { id: "nasdaq100", name: "Nasdaq 100", annualReturnPct: 7, dividendPct: 0.6, builtin: true },
   { id: "schd", name: "SCHD", annualReturnPct: 5, dividendPct: 3.5, builtin: true },
   { id: "vti", name: "VTI (Total US Market)", annualReturnPct: 6, dividendPct: 1.4, builtin: true },
